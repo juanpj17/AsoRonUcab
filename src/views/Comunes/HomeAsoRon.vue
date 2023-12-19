66be546c3ca2ab1ebd74b39942179ced66e18cc4
AndreaCedenoR <139142169+AndreaCedenoR@users.noreply.github.com>
2023-12-19 17:53:33 -0400

    listo el home espero les guste si no no me importa

--- src/views/Comunes/HomeAsoRon.vue
+++ src/views/Comunes/HomeAsoRon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="background-color: var(--fondo);">
       <NavbarAdm v-if="registrado=='%'"></NavbarAdm>
       <NavbarCli v-if="registrado=='$'" > </NavbarCli>
 
@@ -11,27 +11,33 @@
         </b-carousel>
         <!--Ofertas-->
         <b-container>
-          <b-row>
-            <b-col><h3 style="color:var(--vinotinto);margin-top:10px"> ~NUESTRAS OFERTAS~</h3></b-col>
-          </b-row>
+          <div class="diario"></div>
         </b-container>
         
-          <b-container style="margin-top:20px">
+        <b-container style="margin-top:20px">
               <b-row>
                 <b-col cols="4"  v-for="producto in Productos" :key="producto.id">
-          <b-card style="max-width: 50rem;margin-left: 50PX;" class="mb-2">
-            <b-row>
-              <b-col></b-col>
-              <b-col></b-col>
-              <b-col> <b-alert show variant="light">-10%</b-alert></b-col>
-            </b-row>
-            <img :src="producto.Imagen" class="card-img-top">
-            <b-card-title><h3>{{ producto.Nombre }}</h3></b-card-title>
-            <h5 class="card-subtitle mb-2 mr-5 text-body-secondary">{{ producto.Precio }} Bs</h5>
-            <!-- Botón desplegable de opciones -->
-            <b-button href="#" variant="primary" >Ver mas</b-button>
-          </b-card>
-        </b-col>
+                  <b-card style="max-width: 50rem;margin-left: 50PX;" class="mb-2">
+                    <b-row>
+                      <b-col></b-col>
+                      <b-col></b-col>
+                      <b-col> <b-alert show variant="light" class="oferta">{{ producto.Dto }}%</b-alert></b-col>
+                    </b-row>
+                    <img :src="producto.Imagen" class="card-img-top" style="border-bottom: 2px solid var(--vinotinto);">
+                    <b-card-title><h3>{{ producto.Nombre }}</h3></b-card-title>
+                    <div class="d-flex justify-content-evenly align-items-center mb-2">
+                      <h5 class="card-subtitle mb-2 mr-5 text-body-secondary" >{{ producto.Presentacion }} lts</h5>
+                      <h5 class="card-subtitle mb-2 mr-5 text-body-secondary" >{{ producto.GradosA }}</h5>
+                    </div>
+                    <div class="d-flex justify-content-evenly align-items-center mb-2">
+                      <h3 class="card-subtitle mb-2 mr-5 text-body-secondary previo" style="color: #4f4949  !important;">{{ producto.Precio }} Bs</h3>
+                      <h3 class="card-subtitle mb-2 mr-5 text-body-secondary" style="color: var(--verde) !important;">{{ descuento(producto.Dto, producto.Precio)}} Bs</h3>
+                    </div>
+                    
+                    <!-- Botón desplegable de opciones -->
+                    <b-button href="#" variant="primary" >Ver mas</b-button>
+                  </b-card>
+                </b-col>
               </b-row>
           </b-container>
           <!--PIE DE PAGINA-->
@@ -80,6 +86,19 @@
   height: 300px !important;
 }
 
+.previo{
+  text-decoration: line-through;
+  font-size: 25px !important;
+}
+
+.btn-primary {
+  background-color: var(--vinotinto) !important;
+  border-color: var(--vinotinto) !important;
+  background-color: var(--dorado) !important;
+  border-color: var(--dorado) !important;
+}
+
+
 .carousel-control-prev, .carousel-control-next {
   opacity: -1.1 !important;
 }
@@ -103,6 +122,19 @@
   margin: 0 10px; 
 }
 
+.oferta{
+  color: red !important; 
+  border: transparent !important;
+  height: 3rem !important;
+}
+
+.diario{
+  background-image: url('../../assets/DiarioRonero.png'); 
+  background-size: cover;
+  padding: 25px 0; 
+  margin-top: 10px;
+  height: 99px;
+}
 
 /*iconos más grandes*/
 .b-icon.bi {
@@ -150,9 +182,53 @@ import { required } from 'vuelidate/lib/validators';
           require('../../assets/carrusel-img/ron7.jpg')
         ],
        
-        Productos:[{ Id:1,Nombre:'Santa teresa',Precio:20,Presentacion:'0,75',Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg'},{ Id:1,Nombre:'Santa teresa',Precio:20,Presentacion:'0,75',Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg'},
-        { Id:1,Nombre:'Santa teresa',Precio:20,Presentacion:'0,75',Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg'}]
-        
+        Productos:[
+                {
+                  Id:1,
+                  Nombre:'Santa teresa',
+                  Precio:20,
+                  Presentacion:'0,75',
+                  GradosA: '40°', 
+                  Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg',
+                  Dto: 7
+                }, 
+                {
+                  Id:2,
+                  Nombre:'Santa teresa',
+                  Precio:20,
+                  Presentacion:'0,75',
+                  GradosA: '40°', 
+                  Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg',
+                  Dto:5,
+                }, 
+                {
+                  Id:3,
+                  Nombre:'Santa teresa',
+                  Precio:20,
+                  Presentacion:'0,75',
+                  GradosA: '40°', 
+                  Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg',
+                  Dto:10,
+                },
+                {
+                  Id:4,
+                  Nombre:'Santa teresa',
+                  Precio:20,
+                  Presentacion:'0,75',
+                  GradosA: '40°', 
+                  Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg',
+                  Dto: 25,
+                },
+                {
+                  Id:5,
+                  Nombre:'Santa teresa',
+                  Precio:20,
+                  Presentacion:'0,75',
+                  GradosA: '40°', 
+                  Imagen:'https://i.ibb.co/WW33Dwc/gran-reserva-1.jpg',
+                  Dto:10
+                }
+              ]
       }
     },
     
@@ -163,6 +239,10 @@ import { required } from 'vuelidate/lib/validators';
       },
       onSlideEnd(slide) {
         this.sliding = false
+      },
+      descuento(dto, precio){
+        const descuentoCalculado = precio - ((dto / 100) * precio);
+        return descuentoCalculado.toFixed(2);
       }
     }
   }
