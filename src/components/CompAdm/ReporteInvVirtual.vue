90cf1f511de09fad9076cbc4f121d088b0e62e15
JuanPerdomo1 <japerdomo.21@est.ucab.edu.ve>
2024-01-17 19:04:41 -0400

    Listo el consumo a la api

---
+++ src/components/CompAdm/ReporteInvVirtual.vue
@@ -0,0 +1,60 @@
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+    data() {
+        return {
+           month: '',
+           ventas : [],
+           compras: [],
+        }
+    },
+        
+    methods: {
+        //Metodos
+        getGenerarReporteVenta() {
+            const url = 'http://localhost:3000/api/inventario/venta/' + this.month;
+            try {
+                
+                axios.get(url).then(response => {
+                    console.log(response.data);
+                    this.ventas = response.data;
+                })
+
+            } 
+            catch (error) {
+                console.log(error);
+            }
+        },
+
+        getGenerarReporteCompra() {
+            const url = 'http://localhost:3000/api/inventario/compra/' + this.month;
+            try {
+                
+                axios.get(url).then(response => {
+                    console.log(response.data);
+                    this.compras = response.data;
+                })
+
+            } 
+            catch (error) {
+                console.log(error);
+            }
+        },
+    },
+        
+    mounted() {
+        //Cargar datos
+    }
+
+}
+
+
+</script>
+
